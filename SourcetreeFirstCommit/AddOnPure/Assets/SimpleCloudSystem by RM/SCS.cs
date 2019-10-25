8793cb14befa185dbf1deb905cd48910016389e4
JeongeunJPark <jeongeunswd@gmail.com>
2019-10-25 10:33:10 +0900

    New version of 'HarryPotterAndTheARRoom' with AddOnPureCopy file(it contains Google AR Core, manomotion Add-on)

---
+++ SourcetreeFirstCommit/AddOnPure/Assets/SimpleCloudSystem by RM/SCS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HarryPotterAR
+{
+    public class SCS : MonoBehaviour
+    {
+        private Transform player;
+        private float cloudsSpeed = 2f;
+
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!player)
+            {
+                return;
+            }
+            else
+            {
+                gameObject.transform.position = player.transform.position;
+
+                transform.Rotate(0, Time.deltaTime * cloudsSpeed, 0);
+            }
+           
+        }
+    }
+}
---
+++ SourcetreeFirstCommit/AddOnPure/Assets/WeatherPack/Documentation/readme.txt
@@ -0,0 +1,10 @@
+All the prefabs are ready to use just with drag into the scene.
+Make them visible to camera for better performance.
+All prefabs are preset.
+Under prefabs folder locate the lightshower prefab and set its transform rotation x to "-90"
+Under prefabs folder locate the rainingheavily prefab and set its transform rotation y to "-90"
+Under prefabs folder locate the tornado prefab and set its transform rotation x to "-90"
+Under prefabs folder locate the Sun prefab and set its transform rotation x to "-90"
+Demo scene is implemented to test the particles.
+In order to change the direction fo fog change the velocity over lifetime attribute of the particle system.
+To change the size of any prefab change the start size attribute of the particle system.
